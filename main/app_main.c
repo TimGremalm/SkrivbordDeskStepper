e0206c6945995c3c99f34e807fece59021472b74
Tim Gremalm <tim@gremalm.se>
2018-11-19 02:28:12 +0100

    Updated MQTT topics to /Skrivbord/desk

--- main/app_main.c
+++ main/app_main.c
@@ -33,17 +33,17 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event) {
 	switch (event->event_id) {
 		case MQTT_EVENT_CONNECTED:
 			ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-			msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
+			msg_id = esp_mqtt_client_publish(client, "/Skrivbord/desk/hello", "Desk started", 0, 1, 0);
 			ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
 
-			msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
+			msg_id = esp_mqtt_client_subscribe(client, "/Skrivbord/desk/setheight", 0);
 			ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
 
-			msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
-			ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+			//msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+			//ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
 
-			msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
-			ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+			//msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+			//ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
 			break;
 		case MQTT_EVENT_DISCONNECTED:
 			ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
@@ -51,8 +51,8 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event) {
 
 		case MQTT_EVENT_SUBSCRIBED:
 			ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-			msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
-			ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+			//msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+			//ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
 			break;
 		case MQTT_EVENT_UNSUBSCRIBED:
 			ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
